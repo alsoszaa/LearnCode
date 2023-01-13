deab1b3ca491353b793d8b5a46bc145d0a80e3ff
Andrew Alsoszatai-Petheo <Andrew.Alsoszatai-Petheo@oracle.com>
2023-01-12 16:17:37 -0800

    initial upload

---
+++ learn-shell/centos-samba.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Install Samba and allow in firewall:
+dnf install samba samba-client
+systemctl enable --now {smb,nmb}
+firewall-cmd --permanent --add-service=samba
+firewall-cmd --reload
+
+# make a folder if it doesn't exist
+[ ! -d /var/www/html/ha-backup ] && mkdir -p /var/www/html/ha-backup
+
+# append these lines at the end of the /etc/samba/smb.conf file
+tee -a /etc/samba/smb.conf << EOF
+[ha-backups]
+
+comment = Home Assistant Backups
+path = /var/www/html/ha-backup
+browsable = yes
+valid users = alsoszaa
+read only = no
+EOF
+
+#provide ability for my user to access share:
+username='alsoszaa'
+(echo "CisXer5509"; sleep 1; echo "CisXer5509" ) | sudo smbpasswd -s -a $username
